58d8bd9a9b45801947301de49522edacf1cea4fc
shanru <sl706@duke.edu>
2023-08-16 13:00:52 +0800

    fixed evaluate

--- env.sh
+++ env.sh
@@ -4,5 +4,5 @@ export CHECKPOINT_PATH='/data1/temirlan/experiments/test_check/checkpoint-16.pth
 export EPOCH_NUM='200'
 export COMP_TOP='False'
 export COMP_CNCT='True'
-export COMP_TAE='False'
+export COMP_TAE='True'
 export TAU='0.5'
--- functions/evaluate.py
+++ functions/evaluate.py
@@ -15,15 +15,24 @@ def evaluate(data_loader,criterion, model, device):
         targets = batch['ind']
         images = images.to(device, non_blocking=True)
         targets = targets.to(device, non_blocking=True)
-        # compute output
         with torch.cuda.amp.autocast():
-            output = model(images)
-            loss, mse_v, mmt_v = criterion(output, targets)
-
+            #{
+            # output = model(images)
+            # loss, mse_v, mmt_v = criterion(output, targets)
+            outputs,latent = model(images)
+            loss, reconst_v, topo_v, tae_v, cnct_v = criterion(outputs, images, latent, targets)
+            #}
         batch_size = images.shape[0]
         metric_logger.update(loss=loss.item())
-        metric_logger.update(mse=mse_v)
-        metric_logger.update(mmt=mmt_v)
+        #{
+        # metric_logger.update(mse=mse_v)
+        # metric_logger.update(mmt=mmt_v)
+        metric_logger.update(reconst=reconst_v)
+        metric_logger.update(topo=topo_v)
+        metric_logger.update(tae=tae_v)
+        metric_logger.update(cnct=cnct_v)
+        #}
+
 
 
     # gather the stats from all processes
